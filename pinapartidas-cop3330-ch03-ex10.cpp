b69fb1df7399c22abe08f11f50580484007270f7
Rebecca Pina Partidas <rebecca.pinap@gmail.com>
2021-11-29 20:58:05 -0500

    First Commit

---
+++ pinapartidas-cop3330-ch03-ex10.cpp
@@ -0,0 +1,28 @@
+#include "header.h"
+
+int main() 
+{
+    string operation;
+    double operand1;
+    double operand2;
+
+    cout << "Please enter your operation:\nOperation: "; 
+
+    cin >> operation;
+    cout << "\nOperand 1: ";
+    cin >> operand1;
+    cout << "\nOperand 2: ";
+    cin >> operand2;
+    cout << "\n";
+
+    if (operation == "+" || operation == "plus")
+        cout << "The result is " << operand1 + operand2 << ".\n";
+    else if (operation == "-" || operation == "minus")
+        cout << "The result is " << operand1 - operand2 << ".\n";  
+    else if (operation == "*" || operation == "mul")
+        cout << "The result is " << operand1 * operand2 << ".\n";
+    else if (operation == "/" || operation == "div")
+        cout << "The result is " << operand1 / operand2 << ".\n";
+
+    return 0;
+}
